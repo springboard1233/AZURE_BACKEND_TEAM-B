f64dbf756db8312f19e8e2564e1b8efe885711c6
bivinbooster <152492810+bivinbooster@users.noreply.github.com>
2025-10-02 18:55:11 +0530

    Wire new dashboard routes into App shell

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,9 @@ import Overview from './pages/Overview';
 import Analytics from './pages/Analytics';
 import RawData from './pages/RawData';
 import MergedData from './pages/MergedData';
+import FeaturedData from './pages/FeaturedData';
+import PerformanceInsights from './pages/PerformanceInsights';
+import Dashboard from './pages/DashboardPage';
 
 const App: React.FC = () => {
   return (
@@ -17,6 +20,9 @@ const App: React.FC = () => {
             <Route path="/analytics" element={<Analytics />} />
             <Route path="/raw-data" element={<RawData />} />
             <Route path="/merged-data" element={<MergedData />} />
+            <Route path="/featured-data" element={<FeaturedData />} />
+            <Route path="/performance" element={<PerformanceInsights />} />
+            <Route path="/dashboard" element={<Dashboard />} /> {/* ← New */}
           </Routes>
         </main>
       </div>
